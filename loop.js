018df694c1615464fb09e85144f96a6858b0c516
talmagemorris <talmagemorris1@gmail.com>
2020-11-24 23:19:54 -0700

    push

---
+++ loop.js
@@ -0,0 +1,35 @@
+
+
+for (i = 0; i < 10; i++){
+    console.log(i);
+}
+
+for (i = 9; i >= 0 ; i--){
+    console.log(i);
+}
+var fruit = ["banana", "orange", "apple", "kiwi"]
+
+for (i = 0; i < fruit.length; i++){
+    console.log(fruit[i])
+}
+
+arr = [];
+for (i = 0; i < 10; i++){
+    arr.push(i);
+}
+console.log(arr)
+
+for (i = 0; i <= 100; i++){
+    if (i % 2 === 0){
+        console.log(i)
+    }
+}
+fruitArr = [];
+
+var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+for (i = 0; i < fruit.length; i++){
+    if (i % 2 === 0){
+                fruitArr.push(fruit[i])
+                 }
+}
+console.log(fruitArr)
